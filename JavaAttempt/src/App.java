a6694d78bf68236d7c69eb955efb9d7eb60243c1
anna.arutyunova <anna.arutyunova8848@gmail.com>
2022-11-11 17:40:13 -0700

    add round sum

--- JavaAttempt/src/App.java
+++ JavaAttempt/src/App.java
@@ -4,56 +4,11 @@ import java.util.Scanner;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
+import java.math.*;
+import java.math.RoundingMode;
+import java.math.BigDecimal;
+import java.text.DecimalFormat;
 
-// public class App {
-//     public static void main(String[] args) throws Exception {
-//         System.out.println("Hello, World!");
-
-//         int day = 4;
-//         switch(day){
-//             case 1:
-//                 System.out.println("Saturday");
-//                 break;
-//             case 2:
-//                 System.out.println("Tuesday");
-//                 break;
-//             case 5:
-//                 System.out.println("Friday");
-//                 break;
-//             default:
-//                 System.out.println("I don't know what day it is");
-//                 break;
-//         }
-
-//         int i = 1;
-//         do {
-//             System.out.println(i);
-//             i++;
-//         }
-//         while(i<6);
-
-//         for(int a = 0; a <5; a++) {
-//             System.out.println("Yes");
-//         }
-
-//         // Create a new ArrayList
-//         ArrayList<String> arrList = new ArrayList<>();
-
-//         // Add items to it
-//         arrList.add("carrots");
-//         arrList.add("eggs");
-
-//         // Add item of index 0 (so, to the beginning of it). And print it line by line
-//         arrList.add(0,"meat");
-//         System.out.println("ArrayListElements: ");
-//         for (String str:arrList)
-//             System.out.println(str);
-    
-//     }
-
-
-
-// }
 
 // Class Scanner is one way to read a CSV file in java
 public class App
@@ -65,7 +20,9 @@ public class App
         String[] transactions;
         ArrayList<Float> numbers = new ArrayList<>();
         ArrayList<ArrayList<String>> records = new ArrayList<>();
-
+        DecimalFormat df = new DecimalFormat(".##");    
+        
+        
         try{
 
 
@@ -82,28 +39,15 @@ public class App
                         String item = reduced;
                         System.out.println(item);
                         numbers.add(Float.parseFloat(item));
-
-                        // System.out.println(numbers.get(0));
-                        
                     }
                 }
               else{
                         System.out.println("The row is empty");
                     }
-
-            // if((row = reader.readLine()) != null){
-            //     transactions = row.split(splitBy);
-            //     System.out.println(transactions.length);
-            // }
-
-            
-           
             }
+
             double sum = numbers.stream().mapToDouble(n->n).sum();
-            // for(Float number:numbers){
-            //     math.Sum(number);
-            // }
-            System.out.println(sum);
+            System.out.println(df.format(sum));
         }
         catch(IOException e)
         {
